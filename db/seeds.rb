de1e82d2214978bc5081259be62b00b0db1e460f
Ermira Guri <ere.guri@gmail.com>
2024-08-21 17:02:02 +0200

    part of it solved

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,16 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+puts 'Creating restaurants...'
+da_mario = { name: 'Da Mario', address: '15 Gloucester Rd, London SW7 4PP', phone_number: '02075849078', category: 'italian' }
+sumi = { name: 'Sumi', address: '157 Westbourne Grove, London W11 2RS', phone_number: '02045240880', category: 'japanese' }
+gold_mine = { name: 'Gold Mine', address: '100-102 Queensway, London W2 3RR', phone_number: '02077928331', category: 'chinese' }
+balthazar = { name: 'Balthazar', address: '4-6 Russell St, London WC2B 5HZ', phone_number: '02033011155', category: 'french' }
+[da_mario, sumi, gold_mine, balthazar].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
